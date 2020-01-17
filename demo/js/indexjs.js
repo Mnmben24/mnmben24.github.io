1c92db79b909687ad761ce5905a675f53e45ed3b
Marco Benvenuti <marcobenvenuti96@gmail.com>
2020-01-17 14:33:46 +0200

    Working Product Page - Update Java null to /.

--- demo/js/indexjs.js
+++ demo/js/indexjs.js
@@ -62,6 +62,18 @@
                     cursor.continue();
                  }
               };
+              function readAllProducts() {
+                 var objectStore = db.transaction("pumps").objectStore("pumps");
+                 objectStore.openCursor().onsuccess = function(event) {
+                    var cursor = event.target.result;
+
+                    if (cursor) {
+                      var text = cursor.value.title
+                      if (variation != "/.") text += " " + cursor.value.variation;
+                        fillRangeBox(text, cursor.key)
+                       cursor.continue();
+                    }
+                 };
            }
 /*
            function add() {
