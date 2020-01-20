066a29c32541ae71eac6f02c561880538bad9c56
Marco Benvenuti <marcobenvenuti96@gmail.com>
2020-01-20 09:28:38 +0200

    Working Product Page - WIth Ranges n/c

--- demo/sw.js
+++ demo/sw.js
@@ -24,7 +24,7 @@ self.addEventListener('install', function(e) {
 // when the browser fetches a url
 self.addEventListener('fetch', function(event) {
     // either respond with the cached object or go ahead and fetch the actual url
-    event.respondWith(
+  /*  event.respondWith(
         caches.match(event.request).then(function(response) {
             if (response) {
                 // retrieve from cache
@@ -33,5 +33,5 @@ self.addEventListener('fetch', function(event) {
             // fetch as normal
             return fetch(event.request);
         })
-    );
+    );*/
 });
