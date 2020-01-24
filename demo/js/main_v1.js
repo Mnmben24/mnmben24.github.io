e03201f16c4e73941122cf5a90bc4ca93cbb4f95
Marco Benvenuti <marcobenvenuti96@gmail.com>
2020-01-24 12:00:20 +0200

    Cache Testing

--- demo/js/main_v1.js
+++ demo/js/main_v1.js
@@ -3,8 +3,6 @@ var offline = false;
 var registr;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js').then(function(reg) {
-      updateViaCache: 'none'
-      registr = reg;
         console.log('Successfully registered service worker', reg);
     }).catch(function(err) {
         console.warn('Error whilst registering service worker', err);
@@ -21,7 +19,6 @@ window.addEventListener('offline', function(e) {
 
 window.addEventListener('online', function(e) {
     // queue up events for server
-    registr.update();
     console.log("You are online");
     document.getElementsByTagName('body').style = "overscroll-behavior-y: auto;"
     offline = false;
