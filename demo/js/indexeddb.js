a29f5aafc703c55a23c7feadb3c6e54a72adeb43
Marco Benvenuti <marcobenvenuti96@gmail.com>
2020-02-18 08:40:52 +0200

    Fix?

--- demo/js/indexeddb.js
+++ demo/js/indexeddb.js
@@ -75,29 +75,11 @@
              objectStore.openCursor(indx).onsuccess = function(event) {
                 var cursor = event.target.result;
                 var jsonStr = "";
-                var jsn1 = {};
-                var jsn2 = {};
+                var jsn = {};
 
 
                 if (cursor) {
-                  jsonStr = " {'Category' : '" + cursor.value.Category + "','ID' : '" + cursor.value.ID + "','PD' : '" + cursor.value.PD +
-                  "','caption' : '" + cursor.value.caption + "','img' : '" + cursor.value.img + "','range' : '" + cursor.value.range +
-                  "','subtitle' : '" + cursor.value.subtitle + "','title' : '" + cursor.value.title + "','variation' : '" + cursor.value.variation +
-                  "','var1' : '" + cursor.value.var1 + "','var1n' : '" + cursor.value.var1n +
-                  "','var2' : '" + cursor.value.var2 + "','var2n' : '" + cursor.value.var2n +
-                  "','var3' : '" + cursor.value.var3 + "','var3n' : '" + cursor.value.var3n +
-                  "','var4' : '" + cursor.value.var4 + "','var4n' : '" + cursor.value.var4n +
-                  "','var5' : '" + cursor.value.var5 + "','var5n' : '" + cursor.value.var5 + "'}";
 
-                  var jsn = {'Category' : cursor.value.Category ,'ID' : cursor.value.ID,'PD' : cursor.value.PD,
-                  'caption' : cursor.value.caption, 'img' : cursor.value.img , 'range' : cursor.value.range,
-                  'subtitle' : cursor.value.subtitle, 'title' :  cursor.value.title,'variation' : cursor.value.variation ,
-                  'var1' : cursor.value.var1, 'var1n' : cursor.value.var1n,
-                  'var2' : cursor.value.var2, 'var2n' : cursor.value.var2n,
-                  'var3' : cursor.value.var3, 'var3n' : cursor.value.var3n,
-                  'var4' : cursor.value.var4, 'var4n' : cursor.value.var4n,
-                  'var5' : cursor.value.var5, 'var5n' : cursor.value.var5n };
-                    //jsn1.add(jsn);
                     jsn2['Category'] = cursor.value.Category;
                     jsn2['ID'] = cursor.value.ID;
                     jsn2['PD'] = cursor.value.PD;
@@ -112,12 +94,8 @@
                     jsn2['var3'] = cursor.value.var3;  jsn2['var3n'] = cursor.value.var3n;
                     jsn2['var4'] = cursor.value.var4;  jsn2['var4n'] = cursor.value.var4n;
                     jsn2['var5'] = cursor.value.var5;  jsn2['var5n'] = cursor.value.var5n;
-                    var jsonStrNew = JSON.stringify(jsn1);
-                    var str = JSON.stringify(jsn2);
-                    var jsonNew = JSON.parse(str);
-                    var jsonNew1 = JSON.parse(jsonStrNew);
-                    var jsonNew2 = JSON.parse(jsonStr);
-                  changeDetails(jsonStr);
+                    var jsonStr = JSON.stringify(jsn);
+                    changeDetails(jsonStr);
                 }
              };
                objectStore.openCursor().onerror = function(event) {
